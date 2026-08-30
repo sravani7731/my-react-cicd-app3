8172cd9544f3cd99960e3d816065a8afefca33dc
sravani7731 <madugulasravani18@gmail.com>
2026-08-30 13:25:22 +0530

    Initial React commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My React App!</h1>
+      <p>Deployed automatically via GitHub Actions CI/CD to Ubuntu Server.</p>
+    </div>
+  );
+}
+
+export default App;
