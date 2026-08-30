8f31ac54a7808a3bf25bef076a72e44cb44199db
sravani7731 <madugulasravani18@gmail.com>
2026-08-30 16:14:54 +0530

    Added React logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div className="App" style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Welcome to My React App!</h1>
       <p>Deployed automatically via GitHub Actions CI/CD to Ubuntu Server.</p>
+      <img src={logo} className="App-logo" alt="logo" style={{ height: '250px' }} />
     </div>
   );
 }
